988c075421f11a1fb4c2425b14f1263c5eedbd0f
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-05-29 14:52:55 -0400

    prepare actions and reducer

--- src/contexts/useCreatedElements.js
+++ src/contexts/useCreatedElements.js
@@ -1,6 +1,53 @@
 
+export const ACTIONS = {
+  EDIT_HERO: "hero_img",
+  EDIT_TITLE: "hero_title",
+  EDIT_DESCRIPTION: "hero_description",
+  EDIT_BG_COLOR: "bg_color",
+  EDIT_TITLE_FONT: "title_font",
+  EDIT_BODY_FONT: "body_font",
+  ADD_SECTION: "add_section",
+  DELETE_SECTION: "del_section",
+  ADD_ENTRY: "add_entry",
+  DELETE_ENTRY: "del_entry"
+}
 
-export const useCreatedElements = () => {
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ACTIONS.EDIT_HERO:
+      return { ...state, hero_img: action.payload }
+    case ACTIONS.EDIT_TITLE:
+      return { ...state, hero_title: action.payload }
+    case ACTIONS.EDIT_DESCRIPTION:
+      return { ...state, hero_description: action.payload }
+    case ACTIONS.EDIT_BG_COLOR:
+      return { ...state, background_color: action.payload }
+    case ACTIONS.EDIT_TITLE_FONT:
+      return { ...state, title_font: action.payload }
+    case ACTIONS.EDIT_BODY_FONT:
+      return { ...state, body_font: action.payload }
+    case ACTIONS.ADD_SECTION:
+      return { ...state, sections: [...state.sections, action.payload] }
+    case ACTIONS.ADD_ENTRY:
+      return { ...state, entries: [...state.entries, action.payload] }
+    case ACTIONS.DELETE_SECTION:
+      return {
+        ...state,
+        sections: state.sections.filter(section => section !== action.payload)
+      }
+    case ACTIONS.DELETE_ENTRY:
+      return {
+        ...state,
+        entries: state.entries.filter(entry => entry !== action.payload)
+      }
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
+  }
+}
+
+const useCreatedElements = () => {
   const initialState = {
     hero_img: './assets/placeholder.png',
     hero_title: 'Title',
@@ -19,4 +66,6 @@ export const useCreatedElements = () => {
   }
 
 }
+
+export default useCreatedElements;
 
