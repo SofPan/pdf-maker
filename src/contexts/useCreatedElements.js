dbdb1247f1b4924b8c55ac7fc19c4ff05900d0fa
Sofia Panchaud <s.panchaud.89@gmail.com>
2024-05-29 14:41:29 -0400

    set up initialState for app context

--- src/contexts/useCreatedElements.js
+++ src/contexts/useCreatedElements.js
@@ -1,4 +1,22 @@
-/*
-  Update state from form submissions
-  Send state update to Preview Pane
-*/
+
+
+export const useCreatedElements = () => {
+  const initialState = {
+    hero_img: './assets/placeholder.png',
+    hero_title: 'Title',
+    hero_description: 'Lorem, ipsum dolor sit amet consectetur adipisicing',
+    background_color: '#ccc',
+    title_font: 'serif',
+    body_font: 'sans-serif',
+    sections: ["General Fiction", "Sci-fi/Fantasy", "Horror"],
+    entries: [
+      {
+        title: 'sample entry',
+        img_src: './assets/placeholder.png',
+        rating: '4.5',
+        description: 'lorem, ipsum dolor sit amet consectetur adipisicing'
+      }]
+  }
+
+}
+
